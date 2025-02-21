095dea7c7921a9aa6127461e4829d33f5e0e1cf8
Indigo2000 <willirvine@yahoo.co.uk>
2025-02-21 09:38:24 +0000

    Changes to be committed: 	modified:   src/App.jsx     This version provides searchable headlines from Hacker News

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,48 @@
 import * as React from 'react';
 
 const welcome = {
-  greeting: 'Hey',
-  title: 'React'
+  greeting: 'Hacker News Headlines',
+};
+
+const API_ENDPOINT = 'http://hn.algolia.com/api/v1/search?tags=front_page';
+
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case 'STORIES_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      }
+    case 'STORIES_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };     
+    case 'STORIES_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+
+    case 'REMOVE_STORY':
+      return {
+        ...state,
+        data: state.data.filter(
+          (story) => action.payload.objectID !== story.objectID
+        ),
+      };
+    default:
+      throw new Error()
+  }
+  if (action.type === 'SET_STORIES') {
+    
+  } else {
+    throw new Error ();
+  }
 };
 
 const useStorageState = (key, initialState) => {
@@ -18,45 +58,54 @@ const useStorageState = (key, initialState) => {
 
 
 const App = () => {
-
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: ' Jordan Walke ',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https:://redux.js.org/',
-      author: ' Dan Abramov, Andrew Clark ',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
   
-  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
+  const [searchTerm, setSearchTerm] = useStorageState('search', '');
+  const [stories, dispatchStories] = React.useReducer(
+    storiesReducer ,
+     { data: [], isLoading: false, isError: false }
+
+  );
+ 
+
+  React.useEffect(() => {
+    dispatchStories({ type: 'STORIES_FETCH_INIT'});
+
+    fetch(`${API_ENDPOINT}`)
+      .then((response) => response.json())
+      .then(result => {
+        dispatchStories({
+          type: 'STORIES_FETCH_SUCCESS',
+          payload: result.hits,
+      });
+    })
+    .catch(() => 
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' })  
+    );
+  }, []);
+
+  const handleRemoveStory = (item) => {
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item,
+    });
+  };
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
 
   };
 
-  const searchedStories = stories.filter((story) =>
+  const searchedStories = stories.data.filter((story) =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return (
     <div>
       <h1>
-        {welcome.greeting} {welcome.title}</h1>
+        {welcome.greeting}</h1>
 
         <InputWithLabel
         id="search"
-        label="Search"
         value={searchTerm}
         isFocused
         onInputChange={handleSearch} 
@@ -65,30 +114,52 @@ const App = () => {
       </InputWithLabel>
       <hr />
 
-      <List list={searchedStories} />
+      {stories.isError && <p>Something went wrong...</p>}
+
+      {stories.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+
+      <List list={searchedStories} onRemoveItem={handleRemoveStory}/>
+      )}
     </div>
   );
 };
 
-const List = ({ list }) => (
+const List = ({ list, onRemoveItem }) => (
     <ul>
       {list.map((item) => (
-          <Item key={item.objectID} item={item} />
+          <Item 
+          key={item.objectID}
+          item={item}
+          onRemoveItem = {onRemoveItem}
+           />
       )
     )}
   </ul>
   );
 
-const Item = ({item}) => (
+const Item = ({item, onRemoveItem}) => {
+  const handleRemoveItem = () => {
+    onRemoveItem(item);
+  };
+  
+  return (
   <li>
     <span>
       <a href={item.url}> {item.title}</a>
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
+    <span> {item.author} </span>
+    <span> {item.num_comments} </span>
+    <span> {item.points} </span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
   </li>
-)
+);
+};
 
 const InputWithLabel =({id, label, value, type = 'text', onInputChange, isFocused, children}) => {
 
